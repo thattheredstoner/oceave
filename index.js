22ab15d6f56b05beb32d11a17088a490a1b59b6a
ThatTheredstoner <59153074+thattheredstoner@users.noreply.github.com>
2022-05-17 12:28:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+
+function parallaxScroll(){
+   var scrolled = $(window).scrollTop();
+   $('.layer').css('top',(0-(scrolled*.8))+'px');
+   $('.rock-1').css('top',(400-(scrolled*.8))+'px');
+   $('.rock-2').css('top',(200-(scrolled*.6))+'px');
+   $('.rock-3').css('top',(500-(scrolled*.4))+'px');
+   $('.rock-4').css('top',(600-(scrolled*.5))+'px');
+   $('.rock-5').css('top',(600-(scrolled*.7))+'px');
+   $('.rock-6').css('top',(400-(scrolled*.7))+'px');
+   $('.rock-7').css('top',(600-(scrolled*.5))+'px');
+   $('.rock-8').css('top',(200-(scrolled*.2))+'px');
+   $('.rock-9').css('top',(200-(scrolled*.4))+'px');
+}
